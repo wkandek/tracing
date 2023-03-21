2933adc10842c0a7acb32204306dce23d7cd372d
Wolfgang Kandek <wolfgang@kandek.com>
2023-03-21 03:30:18 -0700

    initial upload

---
+++ a.py
@@ -0,0 +1,12 @@
+def add(a,b):
+  return a+b
+
+def subtract(a,b):
+  return a-b
+
+# main
+s = add(1,2)
+s = subtract(s,4)
+print(s)
+
+
---
+++ aa.py
@@ -0,0 +1,27 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer("edpservers")
+
+
+@tracer.start_as_current_span("add")
+def add(a,b):
+  return a+b
+
+@tracer.start_as_current_span("subtract")
+def subtract(a,b):
+  return a-b
+
+# main
+s = add(1,2)
+s = subtract(s,4)
+print(s)
+
+
